f0c935eb816d3739cc834be75d031bc0e0a07f36
Yves <cauayves5@gmail.com>
2023-05-18 01:20:32 -0300

    feat: sign in

--- src/controllers/sign.controller.js
+++ src/controllers/sign.controller.js
@@ -1,33 +1,39 @@
-import { checkIfUserExists, createUser } from "../services/auth.service.js";
+import { checkIfUserExists, createUser, searchUserByEmail } from "../services/auth.service.js";
+import bcrypt from "bcrypt";
 
 
 export async function signUp(req, res) {
     const { name, email, password, confirmPassword } = req.body;
-  
+
     if (password !== confirmPassword) {
-      return res.status(400).send("senhas não coincidem");
+        return res.status(400).send("senhas não coincidem");
     }
-  
+
     try {
-      const userExists = await checkIfUserExists(email);
-      if (userExists) {
-        return res.status(409).send("usuário já existente");
-      }
-  
-      await createUser(name, email, password);
-      res.sendStatus(201);
+        const userExists = await checkIfUserExists(email);
+        if (userExists) {
+            return res.status(409).send("usuário já existente");
+        }
+
+        await createUser(name, email, password);
+        res.sendStatus(201);
     } catch (error) {
-      res.send(error.message);
+        res.send(error.message);
     }
-  }
+}
 
 export async function signIn(req, res) {
 
     const { email, password } = req.body
 
     try {
-
-
+        const userExists = await checkIfUserExists(email);
+        if (!userExists) {
+            return res.status(401).send("usuário não existe");
+        }
+        const user = await searchUserByEmail(email)
+        if (!bcrypt.compareSync(password, user.password)) return res.sendStatus(401)
+        return res.sendStatus(200)
     }
     catch (error) {
         res.send(error.message)
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,21 @@
 import db from "../database/database.connection.js"
+import bcrypt from "bcrypt";
 
 export async function checkIfUserExists(email) {
     const result = await db.query("SELECT * FROM users WHERE email = $1;", [email]);
     return result.rowCount !== 0;
 }
-
 export async function createUser(name, email, password) {
     const query = `INSERT INTO users (name, email, password, createdat) VALUES ($1, $2, $3, to_timestamp($4));`;
     const timestamp = new Date().getTime();
-    const values = [name, email, password, timestamp];
-    await db.query(query, values);
+    const hashedPassword = await bcrypt.hash(password, 5)
+    const values = [name, email, hashedPassword, timestamp]
+    return await db.query(query,values)
+}
+export async function searchAllUsers(){
+    return await db.query("SELECT * FROM users")
+}
+export async function searchUserByEmail(email){
+    const user = await db.query("SELECT * FROM users WHERE email = $1;", [email])
+    return user.rows[0]
 }
