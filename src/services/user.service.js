16ccccd685a2551af7364adc814e4caef4415af7
Yves <cauayves5@gmail.com>
2023-05-21 15:56:40 -0300

    now its fixed

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,18 +2,19 @@ import db from "../database/database.connection.js"
 export async function getVisitorsFromUserProfile(userid) {
     const querystring = `
     SELECT
-    SUM(visitCount) AS visitCount,
-    json_agg(json_build_object(
-        'id', id,
-        'shortUrl', shorturl,
-        'url', url,
-        'visitCount', visitCount
-    )) AS shortenedUrls
+        SUM("visitCount") AS visitCount,
+        json_agg(json_build_object(
+            'id', id,
+            'shortUrl', shorturl,
+            'url', url,
+            'visitCount', "visitCount"
+        )) AS shortenedUrls
     FROM urls
     WHERE userid = $1;
-    `
+`;
+
     const tableData = await db.query(querystring, [userid])
-    
-    if(tableData.rowCount === 0) return null
+
+    if (tableData.rowCount === 0) return null
     return tableData.rows[0]
 }
