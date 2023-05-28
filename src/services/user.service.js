7b911fe843ac16005ac89ce989abfed09e870294
Yves <cauayves5@gmail.com>
2023-05-28 18:18:52 -0300

    fix on format of the table from user/me

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,17 +5,16 @@ export async function getVisitorsFromUserProfile(userid) {
     SELECT
     COALESCE(SUM("visitCount"), 0) AS "visitCount",
     COALESCE(json_agg(json_build_object(
-      'id', id,
-      "shortUrl", "shortUrl",
-      'url', url,
-      "visitCount", "visitCount"
+      id, id,
+      shortUrl, "shortUrl",
+      url, url,
+      visitCount, "visitCount"
       )), '[]'::json) AS "shortenedUrls"
     FROM urls
     WHERE "userId" = $1;
     `
 
   const tableData = await db.query(querystring, [userid])
-  console.log(tableData)
   if (tableData.rowCount === 0) return null
   return tableData.rows[0]
 }
